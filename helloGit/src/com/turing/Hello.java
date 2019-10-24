92a87981eb998f4d1039ae9a182e639bbf95e4ad
dlj <dlj@123.com>
2019-10-24 18:20:02 +0800

    这是dev分支的第一次修改

--- helloGit/src/com/turing/Hello.java
+++ helloGit/src/com/turing/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello Git、GitHub");
         System.out.println("I am idea");
-        System.out.println("我是master分支创建的");
+        System.out.println("我是dev分支创建的");
     }
 }

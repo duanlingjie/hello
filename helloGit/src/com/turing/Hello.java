81cea0964f164add86b0dce54d369605c6ad4707
dlj <dlj@123.com>
2019-10-24 18:19:01 +0800

    这是dev分支的第一次提交

--- helloGit/src/com/turing/Hello.java
+++ helloGit/src/com/turing/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello Git、GitHub");
         System.out.println("I am idea");
+        System.out.println("我是master分支创建的");
     }
 }
---
+++ helloGit/src/com/turing/dev.java
@@ -0,0 +1,7 @@
+package com.turing;
+
+public class dev {
+    public static void main(String[] args) {
+        System.out.println("我是dev分支创建的");
+    }
+}
